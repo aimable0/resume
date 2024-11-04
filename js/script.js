5f5e8a339f1597f84370ac5e7bb5cdf28da32b39
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-11-04 12:58:28 +0200

    refining

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-const text = "Welcome to my resume website! Here you'll find my projects, skills, and contact information.";
+const h2 = document.querySelector('.main h2')
+console.log(h2)
+const text = 'Aimable'
+
 let i = 0;
-const speed = 50; // Speed of typing in milliseconds
-
-function typeWriter() {
-    if (i < text.length) {
-        document.getElementById("text").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+const timer = setInterval(() => {
+    if (i == text.length) {
+        clearInterval(timer)
     }
-}
+    else {
+        h2.textContent += text[i]
+    }
+    i++;
+}, 120);
 
-// Start the typing effect on page load
-window.onload = typeWriter;
+const p = document.querySelector('.animated-text p')
+const text1 = 'A junior software enginner';
+let m = 0;
+const timer1 = setInterval(() => {
+    if (m == text1.length) {
+        clearInterval(timer1);
+    } else {
+        p.textContent += text1[m]
+    }
+    m++;
+}, 100);
